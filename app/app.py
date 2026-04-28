79c1273ba85f216298821cbcbab7329d189ae410
Varun85535 <kvvarunreddy@gmail.com>
2026-04-28 09:58:03 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return jsonify({
-        "message": "Welcome to CloudKart",
+        "message": "Welcome to INDIA",
         "status": "running",
         "version": "v1"
     })
